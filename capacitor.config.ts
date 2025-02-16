315236912653365aa6aae5936d387538fa14aa6e
HASH717 <abdoumtrn@gmail.com>
2025-02-16 22:01:38 +0100

    Update appId & appName

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,8 +2,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.lovable.tenderspro',
-  appName: 'tenderspro-connect',
+  appId: 'com.tenderspro.app',
+  appName: 'TendersPro',
   webDir: 'dist',
   server: {
     url: 'https://74bd72ef-c253-4d7f-87d7-ab46b197b9e5.lovableproject.com?forceHideBadge=true',
